f15027d6637acaa2266af430da41daf3b01a3c02
Pedro Wanderley <wanderley.pedro@outlook.com>
2022-05-03 09:06:49 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [country, setCountry] = useState("");
+  const [user, setUser] = useState({
+    name: "",
+    country: "",
+  });
+
+  useEffect(() => {
+    const userName = localStorage.getItem("userName");
+    const userCountry = localStorage.getItem("userCountry");
+
+    if (userName && userCountry) {
+      setUser({
+        name: userName,
+        country: userCountry,
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log(2)
+  },[user.name])
+
+  function handleRegister(e) {
+    e.preventDefault();
+    if (!name || !country) {
+      return alert('Precisa preencher os campos')
+    }
+    localStorage.setItem("userName", name);
+    localStorage.setItem("userCountry", country);
+    setUser({ name, country });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: "flex", flexDirection: "column", width: 400 }}>
+        <input placeholder="Nome" onChange={(e) => setName(e.target.value)} />
+        <input
+          placeholder="País"
+          onChange={(e) => setCountry(e.target.value)}
+        />
+        <button type="button" onClick={handleRegister}>
+          Cadastrar
+        </button>
+        <p>User name: {user.name}</p>
+        <p>User country: {user.country}</p>
+      </div>
     </div>
   );
 }
